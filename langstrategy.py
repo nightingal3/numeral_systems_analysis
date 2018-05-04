8b573246ee0833a8a3983019aeb6833cb2f62c08
root <root@DESKTOP-VEKGFTE.localdomain>
2018-05-04 14:57:18 -0400

    Most restricted systems added

--- langstrategy.py
+++ langstrategy.py
@@ -1,7 +1,19 @@
 from language_tree import *
+"""
+	num_type meaning:
+	0: restricted approximate
+	1: restricted exact
+	2: body-part
+	3: other base
+	4: vigesimal
+	5: hybrid
+	6: decimal
+"""
 
-def abk():
-    num_type = 5
+
+
+def abk(): 
+    #num_type = 5
     forest = []
     terms = ["gri", "at", "mumek", "mumat", "mufit", "munggwo", "musi", "musyu"]
     term_iter = 0
@@ -19,7 +31,259 @@ def abk():
 
     return forest
 
+def acg():
+    #num_type = 1
+    forest = []
+    terms = ["bague", "chamai", "matalii"]
+
+    for c in range(3):
+	forest.append(make_tree(terms[c], c-1, "ONE"))
+    
+    forest.append(make_tree(terms[3], [4, 100], "HIG"))
+
+    return forest 
+
+def ain():
+    #num_type = 4
+    forest = []
+    terms0 = ["sine", "tu", "re", "ine", "asikne"]
+    
+    for c in range(3):
+	forest.append(make_tree(terms[c], c+1, "ONE"))
+
+    for c in range(3, 6):
+	forest.append(make_tree(terms[c], c+1, "SUC"))
+    #10
+    forest.append(make_tree("wan", ["tu", "asikne"], "MUL")) 	
+    #6-9
+    forest.append(make_tree(["i", "wan"], ["wan", "ine"], "SUB"))
+    forest.append(make_tree(["ar", "wan"], ["wan", "re"], "SUB"))
+    forest.append(make_tree(["tu", "pesan"], ["wan", "tu"], "SUB"))
+    forest.append(make_tree(["sine", "pesan"], ["wan", "sine"], "SUB"))
+    #20
+    forest.append(make_tree("hotne", ["tu", "wan"], "MUL"))
+    
+    for c in range(30, 100, 20):
+	forest.append(make_tree(["wan", "e", "w", "hotne"], ["w", "hotne", "wan"], "MULSUB"))
+
+    for c in range(40, 120, 20):
+	forest.append(make_tree(["w", "hotne"], ["w", "hotne"], "MUL"))
+
+    for c in range(11, 110, 11):
+	forest.append(make_tree(["v", "ikasma", "u"], ["v", "u"], "ADD"))
+
+    #?
+    return forest
+
+
+def ana():
+    #num_type = 1
+    forest = []
+    terms = ["peada", "betacata", "quimisha"]
+
+    for c in range(3):
+	forest.append(make_tree(terms[c], c+1, "ONE"))
+
+    forest.append(make_tree(["quimsha", "beta"], [4, 100], "HIG"))
+
+    return forest
+
+
+def awp():
+    #num_type = 1
+    forest = []
+    terms = ["maza", "pas", "kutna", "ambara"]
+
+    forest = first_three(forest, terms)
+ 
+    forest.append(make_tree("ambara", 4, "SUC"))
+    forest.append(make_tree("ambara", [5, 100], "HIG"))
+
+    return forest
+
+def brs():
+    #num_type = 1
+    forest = []
+    terms = ["sig-u/o", "hua-ra", "idia-ra", "babarirak-u/o", "kohobokarak-u/o"]
+
+    for c in range(6):
+	if c in range(3):
+	    forest.append(make_tree(terms[c], c+1, "ONE"))
+	else:
+	    forest.append(make_tree(terms[c], c+1, "SUC"))
+
+    forest.append(make_tree(terms[4], [6, 100], "HIG"))
+
+    return forest
+
+def bae():
+    #num_type = 1
+    forest = []
+    terms = ["baku-nakaji", "biku-nama", "kijiku-nama"]
+
+    forest = first_three(forest, terms)
+
+    forest.append(make_tree(terms[2], [4, 100], "HIG"))
+
+    return forest
+
+def hix():
+    #num_type = 1
+    forest = []
+    terms = ["towenyxa", "asako", "osorwawo"]
+
+    forest = first_three(forest, terms)
+    
+    forest.append(make_tree(terms[2], [4, 100], "HIG"))
+
+    return forest
+
+def hpd():
+    #num_type = 1
+    forest = []
+    terms = ["?ayup", "ko?ap", "mora?ap", "babni" "?aedapuh"]
+
+    return forest
+
+
+def imo():
+    #num_type = 1
+    forest = []
+    terms = ["mugasl", "sabla", "sabla mugo", "sabla sabla", "sabla sabla mugo"]
+
+    forest = first_three(forest, terms)
+    
+    for c in range(3, 5):
+        forest.append(make_tree(terms[c], c+1, "SUC"))
+
+    forest.append(make_tree(terms[4], [6, 100], "HIG"))
+
+    return forest
+
+def kay():
+    #num_type = 1
+    forest = []
+    terms = ["warngiida", "kiyarrngka", "burldamurra", "mirdinda", "muthaa"]
+
+    forest = first_three(forest, terms)
+    
+    forest.append(make_tree(terms[3], 4, "SUC"))
+    forest.append(make_tree(terms[4], [5, 100], "HIG"))
+
+    return forest
+    
+def myi():
+    #num_type = 1
+    forest = []
+    terms = ["yawumbawa", "yabaranwa", "yabalawa"]
+    
+    forest = first_three(forest, terms)
+    
+    forest.append(make_tree(terms[2], [4, 100], "HIG"))
+
+    return forest
+
+def mrt():
+    #num_type = 1
+    forest = []
+    terms = ["kalika", "kayarra", "jarrkurti", "maruwarla"]
+
+    forest = first_three(forest, terms)
+    
+    forest.append(make_tree(terms[3], [4, 100], "HIG"))
+
+    return forest
+
+
+def pit():
+    #num_type = 1
+    forest = []
+    terms = ["kutju", "kutjara", "mankurpa"]
+    
+    forest = first_three(forest, terms)
+    
+    forest.append(make_tree(terms[2], [4, 100], "HIG"))
+
+    return forest
+
+
+def ram():
+    #num_type = 1
+    forest = []
+    terms = ["saimig", "puksak", "pansak", "kugkugbi", "kwikistar"]
+
+    forest = first_three(forest, terms)
+    
+    for c in range(3, 5):
+        forest.append(make_tree(terms[c], c+1, "SUC")) 
+    
+    forest.append(make_tree(terms[4], [6, 100], "HIG"))
+
+    return forest
+
+def wsk():
+    #num_type = 1
+    forest = []
+    terms = ["itoketa", "itelala", "iteltoke"]
+
+    forest = first_three(forest, terms)
+    
+    forest.append(make_tree(["w", "-", "w"], ["w", "w"], "ADD"))
+    forest.append(make_tree(["w-w", "itoketa"], ["w", "w", "itoketa"], "ADD"))
+    #TODO: make multi-add func
+
+    forest.append(make_tree("kuting dilisan-dilisan", [11, 100], "HIG"))
+    return forest
+
+def wch():
+    #num_type = 1
+    forest = []
+    terms = ["wenyala", "tagw", "najtefwayal", "fwantes ihi", "qwe wenyal", "ipofwustoj", "ipofwusfwaya el", "ipofwusfwantes ihi", "oqwecho taqs"]
+
+    forest = first_three(forest, terms)
+
+    for c in range(3, 9):
+	forest.append(make_tree(terms[c], c+1, "SUC"))
+
+    forest.append(make_tree(terms[8], [11, 100], "HIG"))
+
+    return forest
+
+def yid():
+    #num_type = 1
+    forest = []
+    terms = ["guman", "jambul", "dagul", "yunggan gunyjii", "mala"]
+
+    forest = first_three(forest, terms)
+    
+    for c in range(3, 5):
+        forest.append(make_tree(terms[c], c+1, "SUC"))
+
+    return forest
+
+
+def xoo():
+    #num_type = 1
+    forest = []
+    terms = ["?ua(p)". "num(p)", "ae(p)"]
+
+    forest = first_three(forest, terms)
+
+    forest.append(make_tree(terms[2], [4, 100], "HIG"))
+  
+    return forest
+
+
+
+# helper functions for common language aspects
+
+def first_three(forest, terms):
+    for c in range(3):
+        forest.append(make_tree(terms[c], c+1, "ONE"))
+         
+    return forest
+
 
 if __name__ == "__main__":
-    f = abk()
-    forest_disp(f, "abk")			 
+    f = mrt()
+    forest_disp(f, "mrt")			 
--- language_tree.py
+++ language_tree.py
@@ -3,6 +3,10 @@ from anytree.dotexport import RenderTreeGraph
 from anytree.exporter import DotExporter
 import os
 from langstrategy import *
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 
 def make_tree(form, numexp, op):
 	"""Makes a tree for the given expression, where the left subtree represents the form and the right subtree represents the expression."""
@@ -26,9 +30,12 @@ def make_tree(form, numexp, op):
 		r_exp = Node(numexp - 1, parent=r_root)
 			
 	elif op == "HIG":			  	
-		raise NotImplementedError
+	    l = Node(form, parent=root)
+	    r = Node("...", parent=root)	
+	    r_l = Node(str(numexp[0]), parent=r)
+	    r_r = Node(str(numexp[1]), parent=r)
 	
-	elif (op == "ADD" or op == "SUB") or (op == "MUL" or op == "DIV"):
+	elif op in ["ADD", "SUB", "MUL", "DIV", "POW"]:
             l_root = Node(form[0], parent=root)
             l_c = Node(form[1], parent=l_root)
 	    if op == "ADD":
@@ -36,20 +43,31 @@ def make_tree(form, numexp, op):
 	    elif op == "SUB":
 		r_root = Node("-", parent=root)
 	    elif op == "MUL":
-		r_root = Node(u'\u2715', parent=root)
+		r_root = Node(u'\u2715'.encode("utf-8"), parent=root)
 	    elif op == "DIV":
 		r_root = Node(u'\u00f7', parent=root)
+	    elif op == "POW":
+		r_root = Node(u'\u2191', parent=root)
             r_l_root = Node("m", parent=r_root)
             r_r_root = Node("m", parent=r_root)
-            r_l_c = Node("numexp[0]", parent=r_l_root)
-            r_r_c = Node("numexp[1]", parent=r_r_root)
+            r_l_c = Node(numexp[0], parent=r_l_root)
+            r_r_c = Node(numexp[1], parent=r_r_root)
         	
 
-	elif op == "POW":
-	    raise NotImplementedError
+	elif op == "MTA" or op == "MTS":
+	    l_root = Node(form[2], parent=root)
+            l_l_0 = Node(form[0], parent=l_root)
+            l_l_1 = Node(form[1], parent=l_l_0)
+            l_r = Node(form[3], parent=l_root)
 
-	elif op == "MULADD":
-	    raise NotImplementedError
+            r_root = Node("+", parent=root)
+            r_l_r = Node(u'\u2715'.encode("utf-8"), parent=r_root)
+            r_l_l = Node("m", parent=r_l_r)
+            r_l_l_1 = Node(numexp[0], parent=r_l_l)
+            r_l_r = Node("m", parent=r_l_r)
+            r_l_r_1 = Node(numexp[1], parent=r_l_r)
+            r_r = Node("m", parent=r_root)
+            r_r_1 = Node(numexp[2], parent=r_r)        
     
 	return root
 
@@ -89,17 +107,20 @@ def forest_disp(forest, langcode):
 	return 	
 
 if __name__ == "__main__":
-	build_language("abk", True)
+	f = make_tree("sdf", [3, 20], "HIG")
+	print(disp(f, "hig"))
 	assert False
 	t = make_tree("one", 1, "ONE")
 	t1 = make_tree(["u", "ty"], ["u", 10], "MUL")
 	t2 = make_tree("four", 4, "SUC")
         t3 = make_tree(["w", "teen"], ["w", 10], "ADD")
 	t4 = make_tree("twen", 2, "EQV")
+        t5 = make_tree(["twen", "ty", "-", "one"], [2, 10, 1], "MTA")
 	print(disp(t))
 	print(disp(t1, "eng_xty"))
 	print(disp(t2, "eng_four"))
         print(disp(t3, "eng_teen"))
+        print(disp(t5, "eng_mta"))
 	#print(disp(t4, "eng_eqv"))
         print(calc_complexity_base(t3))
 	print(calc_complexity_base(t2))
